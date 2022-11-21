d33bed6a998ac9b02dcc647b2866629c1fde5c9f
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2022-11-21 12:57:38 -0500

    functions moved

--- index.js
+++ index.js
@@ -12,34 +12,36 @@ console.log(DOMSelectors);
 
 DOMSelectors.button.addEventListener("click", function (card) {
   card.preventDefault();
-  let name = DOMSelectors.name.value;
-  let age = DOMSelectors.age.value;
-  let location = DOMSelectors.location.value;
-  function makeCard() {
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      `<div class="contain">
-    <img src="user icon.jpg" class="user">
-    <h3>Name: ${name}</h3>
-    <h3>Age: ${age} years old</h3>
-    <h3 id="city">At: ${location}</h3>
-    <button class="erase">Erase</button> </div>`
-    );
-    DOMSelectors.name.value = "";
-    DOMSelectors.age.value = "";
-    DOMSelectors.location.value = "";
-  }
-  function erase() {
-    let remove = document.querySelectorAll(".erase");
-    remove.forEach((eachRemove) => {
-      eachRemove.addEventListener("click", (event => {
-        event.target.parentElement.remove();
-      }));
-    });
-  }
   makeCard();
   erase();
 });
+
+function makeCard() {
+  let name = DOMSelectors.name.value;
+  let age = DOMSelectors.age.value;
+  let location = DOMSelectors.location.value;
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<div class="contain">
+  <img src="user icon.jpg" class="user">
+  <h3>Name: ${name}</h3>
+  <h3>Age: ${age} years old</h3>
+  <h3 id="city">At: ${location}</h3>
+  <button class="erase">Erase</button> </div>`
+  );
+  DOMSelectors.name.value = "";
+  DOMSelectors.age.value = "";
+  DOMSelectors.location.value = "";
+}
+
+function erase() {
+  let remove = document.querySelectorAll(".erase");
+  remove.forEach((eachRemove) => {
+    eachRemove.addEventListener("click", (event) => {
+      event.target.parentElement.remove();
+    });
+  });
+}
 
 DOMSelectors.wd.addEventListener("click", function () {
   const addCSS = (css) =>
